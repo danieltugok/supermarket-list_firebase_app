d90209d7f329f0bcb21574df9328ad4d705ff660
Daniel Kogut <daniel.kogut@sidearmsports.com>
2023-04-12 16:19:46 -0500

    feat: firebase connection, add list

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js';
+import { getDatabase, ref, push } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js';
+
+const appSettings = {
+    databaseURL: 'https://playground-4e469-default-rtdb.firebaseio.com/',
+}
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const shoppingListDB = ref(database, 'shoppingList');
+
 const inputFieldEl = document.querySelector('#input-field');
 const addButtonEl = document.querySelector('#add-button');
+const shoppingListEl = document.querySelector('#shopping-list');
+
 
 addButtonEl.addEventListener('click', () => {
   const inputValue = inputFieldEl.value;
-  console.log(inputValue);
+  push(shoppingListDB, inputValue);  
+  clearInputFieldEl();
+  appendItemToShoppingListEl(inputValue);
 });
+
+function clearInputFieldEl() {
+  inputFieldEl.value = '';
+}
+
+function appendItemToShoppingListEl(inputValue) {
+    shoppingListEl.innerHTML += `<li>${inputValue}</li>`;
+}
